0f72aa1b6fd8e1e21839385af187fef0ffddb895
Mariano <marianoramborger@gmail.com>
2022-10-17 18:14:17 -0700

    halfway t arrays

---
+++ DataStructures/Arrays/Implementation/MyArray.js
@@ -0,0 +1,45 @@
+
+class MyArray {
+  constructor() {
+    this.length = 0
+    this.data = {};
+  }
+
+  get(index) { //Access method
+    return this.data[index] ;
+  }
+
+  push(item) { //Push or insert
+    this.data[this.length] = item
+    this.length++ ;
+    return this.length ;
+  }
+
+  pop() {
+    if (this.length > 0) {
+      const item = this.data[this.length -1]
+      delete this.data[this.length -1]
+      this.length-- 
+      return item
+    }
+  }
+
+  delete(index) {
+    const item = this.data[index]
+    this.shiftItems(index)
+    return item
+  }
+
+  shiftItems(index) {
+    for (let i = index; i < this.length -1; i++) {
+  //? At the index of the item to be deleted, make the value equal to that of the next index, shift it
+      this.data[i] = this.data[i+1] //? So the i is deleted.
+    }
+    //? But since we shifted everything to the left, the last items still exists. Gotta remove it.
+    delete this.data[this.length -1] 
+    this.length--
+  }
+
+}
+
+const newArr = new MyArray(0,0)
